2e918d572b703fe4628728be270d2ad1777d6c9d
Maz1mumP0w3r <Matthewjkotkin@gmail.com>
2025-03-12 14:58:09 -0400

    First game

---
+++ Assets/HazardScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HazardScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+transform.position += new Vector3(0.2f,0.1f);
+Random.Range(-1f,1f)
