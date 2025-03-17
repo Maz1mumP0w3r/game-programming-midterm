47af10da7c29a8e3902e1861d7ca1734981e509d
Maz1mumP0w3r <Matthewjkotkin@gmail.com>
2025-03-17 13:29:57 -0400

    Added character movement

--- Assets/HazardScript.cs
+++ Assets/HazardScript.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 
-public class HazardScript : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+public class RandomEnemyMovement : MonoBehaviour
+{ 
+    public float moveSpeed = 2f; // enemy movement speed
+    public float moveInterval = 2f; // time between each movement
+
+    private float timer;
+    private Vector3 targetPosition;
+
     void Start()
     {
-        
+        timer = moveInterval; // Set time interval
+        SetNewTargetPosition(); // Target position
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        timer -= Time.deltaTime; 
+    
+        if (timer <= 0f)
+        {
+            SetNewTargetPosition();
+            timer = moveInterval;
+
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+    
+    void SetNewTargetPosition()
+    {
+
+       float xMove = Random.Range(-1f, 1f);
+       float zMove = Random.Range(-1f, 1f);
+     
+        targetPosition = new Vector3(transform.position.x + xMove, transform.position.z + zMove);
     }
 }
-transform.position += new Vector3(0.2f,0.1f);
-Random.Range(-1f,1f)
---
+++ Assets/playermovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+
+    void Start()
+    {
+      
+
+       rb = GetComponent<Rigidbody2D>();
+    }
+ 
+
+    void Update()
+    {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+    
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+
+          rb.linearVelocity = moveDirection * moveSpeed;
+     }
+
+}
